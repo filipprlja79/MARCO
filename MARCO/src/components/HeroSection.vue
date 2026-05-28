d7dcc6d908751bdc2293267a75ba35962ef79b4b
filipprlja79 <filipprlja79@gmail.com>
2026-05-28 22:16:56 +0200

    first commit

---
+++ MARCO/src/components/HeroSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero">
+    <div class="hero__bg"></div>
+    <div class="hero__smoke"></div>
+    <div class="hero__dark"></div>
+
+    <div class="hero__content">
+      <p class="hero__eyebrow">Traditional Montenegrin Taste</p>
+
+      <img
+        src="../assets/images/logo-marco.png"
+        alt="MARCO Delicatess Selection"
+        class="hero__logo"
+      />
+
+      <h1 class="hero__title">Ukus koji sazrijeva vremenom</h1>
+
+      <p class="hero__text">
+        Njeguški specijaliteti proizvedeni tradicionalnim dimljenjem i prirodnim zrenjem.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import '../assets/styles/hero.css'
+</script>
---
+++ MARCO/src/components/main.js
@@ -0,0 +1,9 @@
+import {createApp} from 'vue'
+import App from '.App.vue'
+import './assets/variables.css'
+import './assets/global.css'
+import './assets/hero.css'
+
+
+createApp(App).mount('#app')
+
